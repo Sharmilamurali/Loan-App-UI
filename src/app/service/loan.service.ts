4766cf9e3235409f810e29d8788d90cfeba1c0b4
sharmi <sha@gmail.com>
2023-03-24 00:28:54 +0530

    new commit1

--- src/app/service/loan.service.ts
+++ src/app/service/loan.service.ts
@@ -11,31 +11,30 @@ export class LoanService {
   constructor(private http: HttpClient) { }
 
   CreateLoan(inputdata: any) {
-    return this.http.post('http://localhost:8080/loan/createLoan', inputdata);
+    return this.http.post('http://localhost:8080/loans/createLoan', inputdata);
   }
 
   ViewAllLoan(): Observable<LoanModel[]> {
-    return this.http.get<LoanModel[]>('http://localhost:8080/loan/viewAllLoan');
+    return this.http.get<LoanModel[]>('http://localhost:8080/loans/');
   }
 
   DeleteLoan(loanNo: any) {
-    return this.http.delete('http://localhost:8080/loan/deleteLoan/' + `${loanNo}`, { responseType: 'text' });
+    return this.http.delete('http://localhost:8080/loans/deleteLoan/' + `${loanNo}`, { responseType: 'text' });
   }
 
   UpdateLoan(loanNo: any, value: any) {
-    return this.http.put(`http://localhost:8080/loan/updateLoan/${loanNo}`, value);
+    return this.http.put(`http://localhost:8080/loans/updateLoan/${loanNo}`, value);
   }
 
   ViewLoanbyId(loanNo: any): Observable<any> {
-    return this.http.get<any>('http://localhost:8080/loan/viewLoanbyId/' + `${loanNo}`);
+    return this.http.get<any>('http://localhost:8080/loans/getLoan/' + `${loanNo}`);
   }
 
   ViewUserLoan(username: string, loanNo: any = "",): Observable<any> {
-    return this.http.get<any>('http://localhost:8080/loan/viewUserLoan/' + `${username}` + '?loanNo=' + `${loanNo}`);
+    return this.http.get<any>('http://localhost:8080/loans/getLoans/' + `${username}` + '?loanNo=' + `${loanNo}`);
   }
 
   Search(loanNo: any = "", firstName: string = "", lastName: string = ""): Observable<any> {
-    return this.http.get<any>('http://localhost:8080/loan/search?' + 'loanNo=' + `${loanNo}` + '&firstName=' + `${firstName}` + '&lastName=' + `${lastName}`);
+    return this.http.get<any>('http://localhost:8080/loans/search?' + 'loanNo=' + `${loanNo}` + '&firstName=' + `${firstName}` + '&lastName=' + `${lastName}`);
   }
-
 }
